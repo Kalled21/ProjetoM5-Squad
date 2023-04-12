466fe0e5b68d238aeac05f540caff91ca0734731
Kalled21 <phh.almeida01@gmail.com>
2023-04-11 21:14:03 -0300

    update

--- index.js
+++ index.js
@@ -1,41 +1,58 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-const morgan = require('morgan');
+const dotenv = require('dotenv');
+const winston = require('winston');
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// configurar o body parser para receber dados via JSON
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
 
-// habilitar o CORS
-app.use(cors());
+// Definir as origens permitidas e os métodos permitidos para o CORS
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:4200'];
+const allowedMethods = ['GET', 'POST', 'PUT', 'DELETE'];
+app.use(cors({
+  origin: allowedOrigins,
+  methods: allowedMethods
+}));
 
-// logar as solicitações HTTP
-app.use(morgan('tiny'));
+// Utilizar o logger winston para logar as requisições HTTP
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+app.use((req, res, next) => {
+  logger.info({
+    method: req.method,
+    url: req.url,
+    headers: req.headers
+  });
+  next();
+});
 
-// definir as rotas
-app.use('/clientes', require('./routes/clientes'));
-app.use('/produtos', require('./routes/produtos'));
-app.use('/pedidos', require('./routes/pedidos'));
-app.use('/distribuidora', require('./routes/distribuidora'));
-app.use('/unidades', require('./routes/unidades'));
-app.use('/funcionarios', require('./routes/funcionarios'));
+app.get('/', (req, res) => {
+  res.send('Olá, mundo!');
+});
 
-// middleware de rota não encontrada
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Rota não encontrada' });
 });
 
-// middleware de erro personalizado
 app.use((err, req, res, next) => {
-  console.error('Erro:', err.message);
+  logger.error({
+    message: err.message,
+    stack: err.stack
+  });
   res.status(500).json({ error: 'Erro interno do servidor' });
 });
 
-// iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor iniciado na porta ${port}`);
 });
+
